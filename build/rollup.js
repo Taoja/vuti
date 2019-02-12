cddc68fb69a3e9ce5b16125eb2e70e4fe1bb926a
huangwutao <346792184@qq.com>
2019-02-12 16:21:13 +0800

    修改cjs为umd输出

--- build/rollup.js
+++ build/rollup.js
@@ -1,28 +1,41 @@
-var rollup = require( 'rollup' );
-var commonjs =  require('rollup-plugin-commonjs')
-var resolve = require('rollup-plugin-node-resolve')
-var postcss = require('rollup-plugin-postcss')
-var url = require('rollup-plugin-url')
+const rollup = require('rollup');
+const commonjs = require('rollup-plugin-commonjs')
+const resolve = require('rollup-plugin-node-resolve')
+const postcss = require('rollup-plugin-postcss')
+const url = require('rollup-plugin-url')
+const uglify = require('rollup-plugin-uglify-es')
+const babel = require('rollup-plugin-babel')
 
 const args = process.argv
 
-const format = args.includes('es') ? 'es' : 'cjs'
-
-console.log(format)
+const format = args.includes('es') ? 'es' : 'umd'
 
 rollup.rollup({
-    input: 'src/index.js',
-    plugins: [
-      commonjs(),
-      resolve(),
-      postcss({extensions: [ '.css' ]}),
-      url({
-        emitFiles: false
-      })
-    ]
-}).then( function ( bundle ) {
-    bundle.write({
-        format: format,
-        file: `dist/vuti.${format}.js`
-    });
+  input: 'src/index.js',
+  plugins: [
+    commonjs(),
+    resolve(),
+    postcss({
+      extensions: ['.css']
+    }),
+    url({
+      emitFiles: false
+    }),
+    uglify(),
+    babel({
+      plugins: ['external-helpers'],
+      babelrc: false,
+      presets: [
+        ['env', {
+          modules: false
+        }]
+      ],
+    })
+  ]
+}).then(function (bundle) {
+  bundle.write({
+    name: 'Vuti',
+    format: format,
+    file: `dist/vuti.${format == 'umd' ? 'min' : format}.js`
+  });
 });
